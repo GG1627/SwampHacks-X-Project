d98b910001f67e11062e601c905e7086cac2e998
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2025-01-25 14:56:25 -0500

    updates to homescreen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -12,12 +12,11 @@ const HomeScreen = () => {
   return (
     <div className="HomeScreen_container">
       <div className="title-bar title-bar2">
-        <button className="button2">Login</button>
+        <button>Login</button>
       </div>
     <div className="HomeScreen_container2">
-
       <div className="window" style={{ width: "700px" }}>
-        <div className="title-bar">
+        <div className="title-bar title-bar3">
           <div className="title-bar-text">Welcome To:</div>
           <div className="title-bar-controls">
             <button aria-label="Minimize"></button>
@@ -37,16 +36,13 @@ const HomeScreen = () => {
           <button role="tab" aria-selected={activeTab === 'tab-B'} aria-controls="tab-B" onClick={() => handleTabClick('tab-B')}>Creators</button>
         </menu>
         <article role="tabpanel" id="tab-A" hidden={activeTab !== 'tab-A'}>
-          <p className="text-title">Tab Content</p>
-          <p>
-            You create the tabs, you would use a <code>menu role="tablist"</code> element then for the tab titles you use a <code>button</code> with the <code>aria-controls</code> parameter set to match the relative <code>role="tabpanel"</code>'s element.
-          </p>
-          <p>
-            Read more at <a href="https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Tab_Role" target="_blank">MDN Web docs - ARIA: tab role</a>
+          <p className="text-title2">What We Are</p>
+          <p className="about">
+            Root & Connect is a hollistic web application designed to support student mental health, activism, and advocacy. Our mission is simple: "Help yourself, help others."
           </p>
         </article>
         <article role="tabpanel" id="tab-B" hidden={activeTab !== 'tab-B'}>
-        <p className="text-title">Tab Content</p>
+        <p className="text-title">What We Are</p>
           <p>
             You create the tabs, you would use a <code>menu role="tablist"</code> element then for the tab titles you use a <code>button</code> with the <code>aria-controls</code> parameter set to match the relative <code>role="tabpanel"</code>'s element.
           </p>
