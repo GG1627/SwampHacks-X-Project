7db74585ae373630fed7fca24538d3bdc60b82f9
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2025-01-25 18:23:35 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 import HomeScreen from "./components/HomeScreen";
 import CalendarPage from "./components/CalendarPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <CalendarPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+      </Routes>
+    </Router>
   );
 }
 
