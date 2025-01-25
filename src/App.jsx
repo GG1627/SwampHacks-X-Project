610ca69e5c54886e6c201171cea1e81e6616b2e4
GG1627 <gael.garcia@ufl.edu>
2025-01-25 16:00:33 -0500

    updated auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import HomeScreen from "./components/HomeScreen";
 import CalendarPage from "./components/CalendarPage";
+import LoginScreen from "./components/LoginScreen";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/login" element={<LoginScreen />} />
       </Routes>
     </Router>
   );
