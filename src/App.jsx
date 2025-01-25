e4ab69b437e48bad7eddac8cd06d01f1a679783c
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2025-01-25 18:14:07 -0500

    fixed main page, added feeling screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./components/HomeScreen";
 import CalendarPage from "./components/CalendarPage";
+import FeelingScreen from "./components/FeelingScreen";
 
 function App() {
   return (
-    <>
-      <CalendarPage />
-    </>
+    <Router>
+      <Routes>
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/calendar" element={<CalendarPage />} />
+      </Routes>
+    </Router>
   );
 }
 
