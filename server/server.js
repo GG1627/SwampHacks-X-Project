3a1c398d7e469318ba58261b536a86e33c2d8eaa
Gael Garcia <gael.garcia@ufl.edu>
2025-01-26 05:43:06 -0500

    added backend

---
+++ server/server.js
@@ -0,0 +1,64 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from the .env file
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse incoming JSON data
+app.use(express.json());
+
+// Enable CORS to allow requests from the frontend
+app.use(cors());
+
+mongoose
+  .connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "Root&Reach",
+  })
+  .then(() => {
+    console.log("Connected to MongoDB using Mongoose");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+// Define Mongoose Schema and Model for User
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+});
+
+// Specify the collection name explicitly
+const User = mongoose.model("User", userSchema, "users"); // Replace "specificCollectionName" with your desired collection name
+
+// Store the username in MongoDB
+app.post("/store-username", async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: "Name is required" });
+  }
+
+  const newUser = new User({ name });
+
+  try {
+    // Create a new user and save it to the database
+    await newUser.save();
+    res
+      .status(201)
+      .json({ message: "Username stored successfully", data: newUser });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error storing username", error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
